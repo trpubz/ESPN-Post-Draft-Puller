53196782ba739fc64ed6409c560709dad042e0d7
trpubz <taylor.pubins@me.com>
2022-04-24 22:13:26 -0700

    working on adding Players to roster spots

--- Player.py
+++ Player.py
@@ -4,15 +4,42 @@
 # created DD MMM YYYY
 # lastUpdate DD MMM YYYY
 
+from collections import namedtuple
+from json import JSONEncoder
+
 
 class Player:
-    espnid: str
+    idESPN: str
+    idFangraphs: str
     name: str
     pos: str
+    tm: str
+    draftedTeam: str
     draftedValue: str
 
-    def __init__(self, espnid, name, pos, draftedValue):
-        self.espnid = espnid
+    def __init__(self, espnid, fgid, name, pos, tm):
+        self.idESPN = espnid
+        self.idFangraphs = fgid
         self.name = name
         self.pos = pos
-        self.draftedValue = draftedValue
+        self.tm = tm
+
+    def set_draft_details(self, tm: str, value: str):
+        self.draftedTeam = tm
+        self.draftedValue = value
+
+    def __eq__(self, other: (str, str)):
+        if self.name == other[0] and self.tm == other[1]:
+            return True
+        else:
+            return False
+
+
+#
+# def customPlayerDecoder(dictPlayer):
+#     return namedtuple('X', dictPlayer.keys())(*dictPlayer.values())
+
+#
+# class PlayerEncoder(JSONEncoder):
+#     def default(self, o):
+#         return o.__dict__
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 # Scrape the ESPN Draft Results webpage and export data to JSON
 # by pubins.taylor
-# v0.2
+# v0.3
 # created 21APR2022
-# lastUpdate 21APR2022
+# loading Player Universe
+# lastUpdate 24APR2022
 
-from Team import *
-import Player
+from TeamOffice import Team
+from Player import *
 from bs4 import BeautifulSoup
-import lxml
+import json
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common import exceptions
 from selenium.webdriver.common.by import By
 
-teams: [Team]
+teams: [Team] = []
+players: [Player] = []
+
+
+def load_Player_Universe():
+    with open("/Users/Shared/Baseball HQ/keysTRP.json", "r") as file:
+        jsonData = json.load(file)
+    print("Datatype of variable: ", type(jsonData))
+    print(jsonData)
+    for i in jsonData:
+        p = Player(espnid=i["idESPN"], fgid=i["idFangraphs"], name=i["name"], pos=i["pos"], tm=i["tm"])
+        players.append(p)
 
 
 def driver_config() -> webdriver:
@@ -33,24 +45,38 @@ def get_recap_html(LeagueId: str) -> str:
     except exceptions as e:
         print(e.message)
     # print(teamTables)
-    return teamTables
     driver.close()
+    return teamTables
 
 
 def parse_tables(recap: str):
-    soup = BeautifulSoup(recap, "lxml")
-    soup.prettify()
+    soup = BeautifulSoup(recap, features="lxml")
     teamTables = soup.select(".InnerLayout__child")
     for team in teamTables:
         teamName = team.find("span", {"class": "teamName truncate"}).get_text()
         tm = Team(name=teamName)
-        # TODO: create player objects from rows
-        tm.roster[RosSpot.C] = Player()
+        table = team.find("tbody")
+        for rowPlayer in table.select("tr"):
+            # a player has 3 table data elements; 1st is overall pick, 2nd is Player data, 3rd is draft value
+            roundData = rowPlayer.select("td")
+            # strip off the $ to get int
+            draftValue = int(roundData[2].get_text().strip("$"))
+            # Player data is segmented into 3 span elements, 1st is name, 2nd is team, 3rd is pos
+            playerName = roundData[1].select("span")[0].get_text()
+            # stip of characters and force uppercase
+            playerTeam: str = roundData[1].select("span")[1].get_text().strip(", ").upper()
+            for plyr in players:
+                if plyr.__eq__(other=(playerName, playerTeam)):
+                    plyr.set_draft_details(tm=teamName, value=draftValue)
+                    tm.add_player(plyr=plyr, draftValue=draftValue)
+
+
         teams.append(tm)
         print(teamName)
     # print(len(teamTables))
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
+    load_Player_Universe()
     recap = get_recap_html(LeagueId="10998")
     parse_tables(recap)
